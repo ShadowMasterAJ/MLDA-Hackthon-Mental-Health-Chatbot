44b04c4d7da81d53cf498f4598f46b7faf5e2352
ShadowAJ <ShadowMasterAJ@users.noreply.github.com>
2021-10-17 05:38:01 +0800

    2nd Commit

--- app.py
+++ app.py
@@ -28,10 +28,7 @@ except:
     for intent in data["intents"]:
         for pattern in intent["patterns"]:
             wrds = nltk.word_tokenize(pattern)
-            # lemmedWrds = [lemmatizer.lemmatize(wrd) for wrd in wrds]
-
             words.extend(wrds)
-            # words.extend(lemmedWrds)
             docs_x.append(wrds)
             docs_y.append(intent["tag"])
 
@@ -88,109 +85,35 @@ except:
     model.save("model.tflearn")
 
 
-# def bag_of_words(s, words):
-#     bag = [0 for _ in range(len(words))]
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
 
-#     s_words = nltk.word_tokenize(s)
-#     s_words = [stemmer.stem(word.lower()) for word in s_words]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
 
-#     for se in s_words:
-#         for i, w in enumerate(words):
-#             if w == se:
-#                 bag[i] = 1
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
 
-#     return np.array(bag)
-
-
-# def chat():
-#     print("Start talking with the bot (type quit to stop)!")
-#     while True:
-#         inp = input("You: ")
-#         if inp.lower() == "quit":
-#             break
-
-#         results = model.predict([bag_of_words(inp, words)])
-#         results_index = np.argmax(results)
-#         tag = labels[results_index]
-
-#         for tg in data["intents"]:
-#             if tg['tag'] == tag:
-#                 responses = tg['responses']
-
-#         print(random.choice(responses))
+    return np.array(bag)
 
 
-# chat()
-nltk.download('punkt')
-nltk.download('wordnet')
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
 
-stemmer = LancasterStemmer()
-lemmatizer = WordNetLemmatizer()
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = np.argmax(results)
+        tag = labels[results_index]
 
-with open("intents.json") as file:
-    data = json.load(file)
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
 
-words = []
-labels = []
-docs_x = []
-docs_y = []
+        print(random.choice(responses))
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        # lemmedWrds = [lemmatizer.lemmatize(wrd) for wrd in wrds]
-
-        words.extend(wrds)
-        # words.extend(lemmedWrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
-
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-words = sorted(list(set(words)))
-
-labels = sorted(labels)
-
-training = []
-output = []
-
-out_empty = [0 for _ in range(len(labels))]
-
-for x, doc in enumerate(docs_x):
-    bag = []
-
-    wrds = [stemmer.stem(w.lower()) for w in doc]
-
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-
-    training.append(bag)
-    output.append(output_row)
-
-training = np.array(training)
-output = np.array(output)
-
-with open("data.pickle", "wb") as f:
-    pickle.dump((words, labels, training, output), f)
-
-tf.compat.v1.reset_default_graph()
-
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
-
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+chat()
---
+++ trainer.py
@@ -0,0 +1,83 @@
+import pickle
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem import WordNetLemmatizer
+import json
+import random
+import tensorflow as tf
+import tflearn
+import numpy as np
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+
+stemmer = LancasterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        # lemmedWrds = [lemmatizer.lemmatize(wrd) for wrd in wrds]
+
+        words.extend(wrds)
+        # words.extend(lemmedWrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
+
+with open("data.pickle", "wb") as f:
+    pickle.dump((words, labels, training, output), f)
+
+tf.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net, tensorboard_verbose=3)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True,)
+print(model.get_weights(net.W))
+model.save("model.tflearn")
